687d86cf455778f9e17b93727b27e52f83f1e8dc
mae616 <oya2.mi@hotmail.co.jp>
2021-12-14 20:36:31 +0900

    カウンターの表示

--- redux-sample/src/components/Counter/presentation.jsx
+++ redux-sample/src/components/Counter/presentation.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Counter = props => {
-    console.log(props);
-
-    return (
-        <div>
-            <div>counter</div>
-        </div>
-    );
-};
+const Counter = ({ count, increment, decrement }) => (
+    <>
+        <div>{count}</div>
+        <button onClick={() => increment(1)}>+</button>
+        <button onClick={() => decrement(1)}>-</button>
+        {/* <button onClick={() => increment(-1)}>-</button> */}
+    </>
+);
 
 export default Counter;
