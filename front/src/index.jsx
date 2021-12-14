c8e20910c33474899a229c6a7290010e38cfd612
mae616 <oya2.mi@hotmail.co.jp>
2021-12-14 21:31:50 +0900

    カレンダーの UI を作成

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <di>hello react!!</di>;
+import CalendarBoard from './components/CalendarBoard';
+
+const App = () => (
+    <di>
+        <CalendarBoard />
+    </di>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
