17485aae4a5f43c6e6ef97cab7b6478fad915347
mae616 <oya2.mi@hotmail.co.jp>
2021-12-15 19:17:02 +0900

    配列を表示する

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,50 +1,34 @@
 import React from "react";
 
 import { GridList } from '@material-ui/core';
-// import { StylesContext } from '@material-ui/core/node_modules/@material-ui/styles';
 import * as styles from './style.css';
 
+import dayjs from 'dayjs';
+import 'dayjs/locale/ja';
+
+dayjs.locale('ja');
+
+
+const createCalendar = () => {
+
+    const firstDay = dayjs().startOf('month');
+
+    // 最初の日の曜日のindexを取得
+    const firstDayIndex = firstDay.day();
+
+    return Array(35)
+        .fill(0)
+        .map((_, i) => i - firstDayIndex);
+};
+
 // ダミーデータ
-const calendar = [
-    "29",
-    "30",
-    "10月1日",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "11月1日",
-    "2"
-];
+const calendar = createCalendar();
 
 
 const CalendarBoard = () => {
+
+    console.log(calendar);
+
     return (
         <div className={styles.container}>
             <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
