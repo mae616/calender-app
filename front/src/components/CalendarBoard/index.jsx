0387d91dd1fa9bedd0627377ef55c1e746a465e0
mae616 <oya2.mi@hotmail.co.jp>
2021-12-15 20:17:19 +0900

    曜日の表示とスタイリング

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import { ImageList } from '@material-ui/core';
+import {
+    ImageList,
+    Typography
+} from '@material-ui/core';
 
 import CalendarElement from '../CalendarElement';
 import { createCalendar } from '../../services/calendar'
 import * as styles from './style.css';
 
 const calendar = createCalendar();
+
+const week = ['日', '月', '火', '水', '木', '金', '土'];
 
 const CalendarBoard = () => {
 
@@ -15,6 +20,19 @@ const CalendarBoard = () => {
     return (
         <div className={styles.container}>
             <ImageList className={styles.grid} cols={7} gap={0} rowHeight="auto">
+                {week.map(dayOfWeek => (
+                    <li key={dayOfWeek}>
+                        <Typography
+                            className={styles.days}
+                            color="textSecondary"
+                            align="center"
+                            variant="caption"
+                            component="div"
+                        >
+                            {dayOfWeek}
+                        </Typography>
+                    </li>
+                ))}
                 {calendar.map(day => (
                     <li key={day.toISOString()}>
                         <CalendarElement day={day}></CalendarElement>
