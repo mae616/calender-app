cabe56ecdc72492a93618464ba8f90f72ad0bb39
mae616 <oya2.mi@hotmail.co.jp>
2021-12-15 20:28:43 +0900

    今日のスタイルを変更

--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -2,14 +2,20 @@
 import React from 'react';
 
 import * as styles from './style.css';
-
 import { Typography } from '@material-ui/core';
+import dayjs from 'dayjs';
 
 const CalendarElement = ({ day }) => {
 
     // 各月の1日は月情報をつける
     const isFirstDay = day.date() === 1;
     const format = isFirstDay ? 'M月D日' : 'D';
+
+    // 当日かどうかの判断
+    const today = dayjs();
+    const compareFormat = 'YYYYMMDD';
+    // 文字列に変換した上で全く同じ文字列になっているか（＝同じ日か）という判定
+    const isToday = day.format(compareFormat) === today.format(compareFormat);
 
     return (
         <div className={styles.element}>
@@ -19,7 +25,9 @@ const CalendarElement = ({ day }) => {
                 variant='caption'
                 component='div'
             >
-                {day.format(format)}
+                <span className={isToday ? styles.today : ''}>
+                    {day.format(format)}
+                </span>
             </Typography>
         </div>
     );
