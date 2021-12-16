f14b5ae771d8afc658296f3391cd679dde846c9a
mae616 <oya2.mi@hotmail.co.jp>
2021-12-16 21:00:17 +0900

    次の月前の月: container に状態の変更を dispatch する関数を定義

--- front/src/components/Navigation/container.jsx
+++ front/src/components/Navigation/container.jsx
@@ -2,11 +2,31 @@ import Navigation from './presentation';
 
 import { connect } from 'react-redux';
 
-const mapStateToProps = state => ({});
+import { getNextMonth, getPreviousMonth } from '../../services/calendar';
+import { calendarSetMonth } from '../../redux/calendar/actions';
 
-const mapDispatchToProps = dispatch => ({});
+const mapStateToProps = state => ({ calendar: state.calendar });
 
-const mergeProps = (stateProps, dispatchProps) => ({});
+// mapDispatchToPropsは connect の第二引数の関数で、
+// dispatch を使う props として渡す関数を定義するためのもの
+// dispatchの際に state が必要になる
+const mapDispatchToProps = dispatch => ({
+    setMonth: month => {
+        dispatch(calendarSetMonth(month));
+    }
+});
+
+// mergePropsでmapStateToPropsとmapDispatchToPropsの結果を使ってごにょごにょしている
+const mergeProps = (stateProps, dispatchProps) => ({
+    setNextMonth: () => {
+        const nextMonth = getNextMonth(stateProps.calendar);
+        dispatchProps.setMonth(nextMonth);
+    },
+    setPreviousMonth: () => {
+        const previousMonth = getPreviousMonth(stateProps.calendar);
+        dispatchProps.setMonth(previousMonth);
+    }
+});
 
 export default connect(
     mapStateToProps,
