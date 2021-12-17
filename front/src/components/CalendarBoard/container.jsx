f43503ae6f0b99aaa8a40354293642bc06546f3c
mae616 <oya2.mi@hotmail.co.jp>
2021-12-17 20:46:36 +0900

    dialogをreduxでの状態管理で開けられるようにする

--- front/src/components/CalendarBoard/container.jsx
+++ front/src/components/CalendarBoard/container.jsx
@@ -3,18 +3,31 @@ import { connect } from 'react-redux';
 import CalendarBoard from './presentation';
 import { createCalendar } from '../../services/calendar';
 
+import { addScheduleOpenDialog } from '../../redux/addSchedule/actions';
+
 // stateをpropsにマッピングしている
 // ※状態の変更の監視を行なって、store が何かひとつでも変更されると、どれが変更されたかによらず実行される
 const mapStateToProps = state => ({ calendar: state.calendar });
+
+const mapDispatchToProps = dispatch => ({
+    openAddScheduleDialog: () => {
+        dispatch(addScheduleOpenDialog());
+    }
+})
 
 // mergeProps...mapStateToPropsで生成されたpropsと
 //              mapDispatchToPropsで生成されたpropsを
 //             引数にとり、コンポーネントで使う形に整形して渡す関数
 // stateProps...mapStateToPropsのこと
 // ※mergePropsはmapStateToPropsの結果が前回と異なっていたときにだけ実行される
-const mergeProps = stateProps => ({
+const mergeProps = (stateProps, dispatchProps) => ({
+    ...stateProps,
+    ...dispatchProps,
     month: stateProps.calendar,
     calendar: createCalendar(stateProps.calendar)
 });
 
-export default connect(mapStateToProps, null, mergeProps)(CalendarBoard);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+    mergeProps)(CalendarBoard);
